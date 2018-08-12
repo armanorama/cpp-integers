ec5219c61a25aedd1c8828a2b2c2df2c519a46d3
mynameisarman <>
2018-08-12 14:45:04 -0400

    Integer types, min/max val. Lesson: INT_MAX/INT_MIN are C, std::numeric_limits<int>::max() and std::numeric_limits<int>::min() are C++. Former isn't being picked up by @quark-zju's vim-cpp-auto-include script, and deletes manual addition of limits include

--- cpp-integers.cpp
+++ cpp-integers.cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main() {
 
   int value = 578;
-  int maxVal = std::numeric_limits<int>::max();
-  int minVal = std::numeric_limits<int>::min();
+  int maxVal = std::numeric_limits<int>::max(); // Tut calls for INT_MAX here, but vim-cpp-auto-include not picking it up and autodeletes the limits include
+  int minVal = std::numeric_limits<int>::min(); // Tut calls for INT_MIN here, but vim-cpp-auto-include not picking it up and autodeletes the limits  include
   cout << value << endl;
 
   cout << "A value: " << value << endl;
